aca2ab65800f4fb9f1fad7b9f7df271c59c9e9bb
Ronald Kasagga <ronald.kasagga@gmail.com>
2024-01-26 02:15:55 +0300

    more flexible api

--- http/handler.go
+++ http/handler.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"context"
+	"strings"
 
 	"github.com/kod2ulz/gostart/api"
 	"github.com/kod2ulz/gostart/logr"
@@ -13,13 +14,15 @@ type Param interface {
 	ContextKey() string
 }
 
-func GenericHandler[P Param, R any](ctx context.Context, log *logr.Logger, method, url string) func(context.Context) (out R, err api.Error) {
+func GenericHandler[P Param, R any](ctx context.Context, log *logr.Logger, method string, url ...string) func(context.Context) (out R, err api.Error) {
 	var err error
 	var res api.Response[R]
 	var param api.RequestParam
 
 	return func(ctx context.Context) (out R, apiErr api.Error) {
-		if param, err = (*new(P)).ContextLoad(ctx); err != nil {
+		if len(url) == 0 {
+			return out, api.RequestLoadError[P](errors.Errorf("invalid url"))
+		} else if param, err = (*new(P)).ContextLoad(ctx); err != nil {
 			return out, api.RequestLoadError[P](errors.Wrapf(err, "Failed to load %T from context", param))
 		} else if param == nil {
 			var ok bool
@@ -27,7 +30,7 @@ func GenericHandler[P Param, R any](ctx context.Context, log *logr.Logger, metho
 				return out, api.RequestLoadError[P](errors.Errorf("Got %v when loading %T from context", param, param))
 			}
 		}
-		if res = Client[R](log.Entry).Body(&param).Request(ctx, method, url); res.HasError() {
+		if res = Client[R](log.Entry).Body(&param).Request(ctx, method, strings.Join(url, "/")); res.HasError() {
 			return out, res.Error
 		} else if err = res.ParseDataTo(&out); err != nil {
 			return out, api.GeneralError[R](errors.Wrapf(err, "failed to parse %T to %T", res.Data, out)).
