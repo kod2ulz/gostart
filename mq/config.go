0ef51e4070c815b241927b8c7fd8475254d149b5
Ronald Kasagga <ronald.kasagga@gmail.com>
2024-08-10 14:16:28 +0300

    update rmq config to support vhosts

--- mq/config.go
+++ mq/config.go
@@ -98,13 +98,13 @@ func Config(prefix ...string) *Conf {
 
 func (c *Conf) ConnectionString() string {
 	return fmt.Sprintf(
-		"%s://%s:%s@%s:%s/",
+		"%s://%s:%s@%s:%s/%s",
 		c.Protocol,
 		c.Username,
 		c.Password,
 		c.Host,
 		c.Port,
-	)
+		strings.Trim(c.Vhost, "/"))
 }
 
 func (c *Conf) String() string {
